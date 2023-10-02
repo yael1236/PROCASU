f86ed356fd78bad52bb4ad33a1e035635e6a8dee
Betzywj <136661742+Betzywj@users.noreply.github.com>
2023-10-01 22:40:56 -0600

    Add files via upload
    Nav, menu, footer

---
+++ nav.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style-nav.css">
+    <title>Nav</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg  ">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo Procasu.png" alt="logo" width="150px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <div class="">
+                    <p class="text-sm-left">PROGRAMA PARA EL DESARROLLO DE LA SUSTENTABLIDAD ENLA CADEIA DE SUMANISTRO</p>
+                </div>
+        </div>
+        <ul class="navbar-nav ml-auto mt-2 mt-lg-0 content">
+            <li class="nav-item dropdown">
+                <a class="nav-link text-dark dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="Iconos/Icono-Usuario.png " class="img-fluid rounded-circle avatar mr-2" alt="imagen" />
+                    usuario
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Mi perfil</a>
+                    <a class="dropdown-item" href="#">Expediente</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Cerrar sesión</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+    <div id="sidebar-container" class=" d-inline-block ">
+        <div class="menu ">
+            <br>
+            <a href="#" class="m-4 d-inline-block btn  d-block  " style="padding:1rem; background-color: var(--procasu3);  ">
+                <img src="Iconos/Icono-Herramientas-PROCASU.png" alt="herramientas" class="iconos ">
+                Herramientas PROCASU
+            </a>
+            <br>
+            <a href="#" class="m-4 d-inline-block btn d-block " style="padding:1rem; background-color: var(--procasu3);  ">
+                <img src="Iconos/Icono-Biblioteca-Digital.png" alt="herramientas" class="iconos ">
+                Biblioteca Digital
+            </a>
+            <br>
+            <a href="#" class="m-4 d-inline-block btn d-block " style="padding:1rem; background-color: var(--procasu3);  ">
+                <img src="Iconos/Icono-Conoce-PROCASU.png" alt="herramientas" class="iconos ">
+                Conoce PROCASU
+            </a>
+            <br>
+            <a href="#" class="m-4 d-inline-block btn  d-block " style="padding:1rem; background-color: var(--procasu3);  ">
+                <img src="Iconos/Icono-Necesitas-Ayuda.png" alt="herramientas" class="iconos ">
+                ¿Necesitas Ayuda?
+            </a>
+        </div>
+    </div>
+
+
+
+
+
+
+    <!-- Footer -->
+    <footer class="footer ">
+        <div class="row">
+
+            <div class="conbnt  ">
+                <a class=" icon ">
+                    <img src="Iconos/Icono-de-dirección---ubicación.png" alt="Ubicación" width="30px">
+                </a>
+                <a>
+                    <p class="texto text-uppercase font-weight-bold  " style="color: var(--procasu1); margin-right: 2rem;"> monterrey</p>
+                </a>
+                <a>
+                    <p class="largo ">Ricardo Margain 575, Parque Corp. Santa Engracia, San Pedro Garza García, N.L. 66267 - Edificio C Piso 3.
+                    </p>
+                </a>
+                <a class="icon">
+                    <img src="Iconos/Icono-Telefono.png" alt="Telefono" width="30px">
+                </a>
+                <a>
+                    <p class="largo1 ">55-4123-29116</p>
+                </a>
+                <a class="icon">
+                    <img src="Iconos/Icono-Correo.png" alt="Correo" width="30px">
+                </a>
+                <a>
+                    <p class="texto texto">atencion@procasu.com.mx</p>
+                </a>
+                <a class="icon">
+                    <img src="Iconos/Icono-Sitio-web.png" alt="Web" width="30px">
+                </a>
+                <a>
+                    <p class="texto">http://www.procasu.net</p>
+                </a>
+            </div>
+            <div class="cont">
+                <a class="">
+                    <img src="img/logo Procasu.png" alt="Logo" width="150px" style="margin-left: 2rem;">
+                </a>
+                <a class="">
+                    <p class="text-sm-left derechos">PROGRAMA PARA EL DESARROLLO DE LA SUSTENTABLIDAD ENLA CADEIA DE SUMANISTRO</p>
+                </a>
+                <a class="m-3">
+                    <small style="color: var(--procasu3); margin-top: 3rem; margin-left: 3rem;">&copy; 2023-2024 <b>Procasu</b> Todos los Derechos Reservados.</small>
+                </a>
+            </div>
+
+        </div>
+
+
+
+    </footer>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+
+</html>
