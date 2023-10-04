cb2c64e88e4a865a0f1411cad1d02d8378e0fe2d
Betzywj <136661742+Betzywj@users.noreply.github.com>
2023-10-03 21:30:18 -0600

    Add files via upload
    modificación y actualización

---
+++ footer.php
@@ -0,0 +1,48 @@
+
+<footer class="footer  ">
+    <div class="row">
+
+        <div class="conbnt ">
+            <a class=" icon1 ">
+                <img src="Iconos/Icono-de-dirección---ubicación.png" alt="Ubicación" width="30px" style="margin-left: 4rem; margin-top: 1.5rem;  ">
+            </a>
+            <a style="margin-right: 1rem;">
+                <p class="texto text-uppercase font-weight-bold  " style="color: var(--procasu1); margin-top: 1rem; "> monterrey</p>
+            </a>
+            <a>
+                <p class="largo ">Ricardo Margain 575, Parque Corp. Santa Engracia, San Pedro Garza García, N.L. 66267 - Edificio C Piso 3.
+                </p>
+            </a>
+            <a class="icon">
+                <img src="Iconos/Icono-Telefono.png" alt="Telefono" width="30px">
+            </a>
+            <a>
+                <p class="largo1 " style="margin-top: .5rem;">55-4123-29116</p>
+            </a>
+            <a class="icon">
+                <img src="Iconos/Icono-Correo.png" alt="Correo" width="30px">
+            </a>
+            <a>
+                <p class="texto texto">atencion@procasu.com.mx</p>
+            </a>
+            <a class="icon">
+                <img src="Iconos/Icono-Sitio-web.png" alt="Web" width="30px">
+            </a>
+            <a>
+                <p class="texto">http://www.procasu.net</p>
+            </a>
+        </div>
+        <div class="cont">
+            <a class="">
+                <img src="img/Logotipo Blanco.png" alt="Logo" width="150px" style="margin-left: 2rem;">
+            </a>
+            <a class="">
+                <p class="text-sm-left derechos">PROGRAMA PARA EL DESARROLLO DE LA SUSTENTABLIDAD ENLA CADEIA DE SUMANISTRO</p>
+            </a>
+            <a class="m-3">
+                <small style="color: var(--procasu3); margin-top: 3rem; margin-left: 3rem;">&copy; 2023-2024 <b>Procasu</b> Todos los Derechos Reservados.</small>
+            </a>
+        </div>
+
+    </div>
+</footer>
